c68f769f75db1aedad6c8982eafdca74aadce1eb
BNgan3C33 <lokenguyen2307@gmail.com>
2023-11-15 16:33:45 +0700

    update paginatione, user_role

--- saleappv1/app/dao.py
+++
@@ -1,9 +0,0 @@
-from app.models import Product, Category
-def load_categories():
-    return Category.query.all()
-
-def load_products(kw=None):
-    products = Product.query
-    if kw:
-        products = products.filter(Product.name.contains(kw))
-    return products
